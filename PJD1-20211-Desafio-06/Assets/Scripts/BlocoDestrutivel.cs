fd2b4130053dae7a82561f04a3eda40ad2fd1a62
Kaploy <80787463+Kaploy@users.noreply.github.com>
2021-06-29 18:09:11 -0400

    scenery_destruction_mechanics (#12)
    * Add files via upload
    
    * Add files via upload

---
+++ PJD1-20211-Desafio-06/Assets/Scripts/BlocoDestrutivel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlocoDestrutivel : MonoBehaviour
+{
+    public int blockHealth = 100;
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage()
+    {
+        blockHealth -= 50;
+
+        if(blockHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ PJD1-20211-Desafio-06/Assets/Scripts/GerarNiveis.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarNiveis : MonoBehaviour
+{
+    public Transform pontoInicial;
+    public GameObject salas;
+    
+
+    public float deslocamento;
+
+    private float intervaloEntreSalas;
+    public float intervaloInicialSalas = 0.25f;
+
+    public float bordaDireita;
+   
+    private bool terminarNivel;
+    void Start()
+    {
+        transform.position = pontoInicial.position;
+        Instantiate(salas, transform.position, Quaternion.identity);
+
+    }
+
+    private void Update()
+    {
+        if (intervaloEntreSalas <= 0 && terminarNivel == false)
+        {
+            Mover();
+            intervaloEntreSalas = intervaloInicialSalas;        
+        }
+        else 
+        {
+            intervaloEntreSalas -= Time.deltaTime;
+        }
+
+    }
+
+    private void Mover()
+    {
+     
+            if (transform.position.x < bordaDireita)
+            {
+                transform.position += Vector3.right * deslocamento;
+                
+            }
+            else
+            {
+                terminarNivel = true; 
+            }
+        
+
+        
+    
+
+        Instantiate(salas, transform.position, Quaternion.identity);
+    }
+}
