f1dd5bad80894858dc3d8a60dd1455b34b7ac2d4
nickolasms <nick.machadosilva@gmail.com>
2021-07-07 12:52:37 -0300

    nickolas final

---
+++ PJD1-20211-Desafio-06/Assets/Scripts/MachineGunDTO2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MachineGunDTO2", menuName = "ScriptableObjects/MachineGunDTO2")]
+public class MachineGunDTO2 : WeaponDTO
+{
+    public float Accuracy;
+    public float DeltaDistance;
+}
+
---
+++ PJD1-20211-Desafio-06/Assets/Scripts/ShotgunDTO2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShotgunDTO2", menuName = "ScriptableObjects/ShotgunDTO2")]
+public class ShotgunDTO2 : WeaponDTO
+{
+    public int Pellet;
+    public float Spread;
+}
